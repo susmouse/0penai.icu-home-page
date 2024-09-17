200873794ab548f714f2d54ddb9de5ef833c0461
tutrabbit <524350588@qq.com>
2024-09-18 04:36:08 +0800

    v3.4.2 导入gsap

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import { gsap } from 'gsap'
 
-createApp(App).use(ElementPlus).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus)
+app.config.globalProperties.$gsap = gsap
+app.mount('#app')
